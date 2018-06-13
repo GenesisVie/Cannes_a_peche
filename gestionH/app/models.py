d66c37fa08918b2a5f2f8f0f5417a5f438a91e34
papalouis <papa.louis59@gmail.com>
2018-06-13 17:50:40 +0200

    connexion done + affichage hotel

--- gestionH/app/models.py
+++ gestionH/app/models.py
@@ -1,11 +1,25 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     statut = db.Column(db.Boolean, unique=True, default=True)
     password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 def __repr__(self):
@@ -24,4 +38,4 @@ class Hotel(db.Model):
 
 
 def __repr__(self):
-    return '<Hotel {}>'.format(self.body)
+    return '<Hotel {}>'.format(self.name)
